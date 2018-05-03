0ef61cede79d4364cf46059c0f6d60d02ea95b28
mikomoares <38050694+mikomoares@users.noreply.github.com>
2018-05-03 20:54:19 -0300

    Add files via upload
    classe cavalo com aceleração e velocidade máxima

---
+++ classecavalinho.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 13:26:42 2018
+
+@author: mihan
+"""
+import pygame
+
+class Cavalinho(pygame.sprite.Sprite):
+  
+  def __init__(self, arquivo_imagem, pos_x, pos_y, vel_x, aceleracao, velocidade_max):
+    pygame.sprite.Sprite.__init__(self)
+    self.vx = vel_x
+    imagem=pygame.image.load(arquivo_imagem)
+    self.image = pygame.transform.scale(imagem,(193,133))
+    self.rect = self.image.get_rect()
+    self.rect.x = pos_x
+    self.rect.y = pos_y
+    self.aceleracao=aceleracao
+    self.max= velocidade_max
+    
+  def move(self):
+    self.rect.x += self.vx
+    
+  def acelera(self):
+    self.vx += self.aceleracao
