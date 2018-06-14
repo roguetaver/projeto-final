c7ae612db2677e78f5c1b8e4ef61216d77749162
roguetaver <38052553+roguetaver@users.noreply.github.com>
2018-06-14 00:29:53 -0300

    funcao mov

--- funcaomov.py
+++ funcaomov.py
@@ -6,20 +6,20 @@ Created on Mon May  7 00:31:53 2018
 """
 
 def movimento (cavalinho1,cavalinho2,cavalinho3,cavalinho4,cavalinho5,cavalinho6):
-    if cavalinho1.rect.x < 1084:
+    if cavalinho1.rect.x < 1465:
         cavalinho1.correndo()
     
-    if cavalinho2.rect.x < 1084:
+    if cavalinho2.rect.x < 1465:
         cavalinho2.correndo()
     
-    if cavalinho3.rect.x < 1084:
+    if cavalinho3.rect.x < 1465:
         cavalinho3.correndo()
         
-    if cavalinho4.rect.x < 1084:
+    if cavalinho4.rect.x < 1465:
         cavalinho4.correndo()
     
-    if cavalinho5.rect.x < 1084:
+    if cavalinho5.rect.x < 1465:
         cavalinho5.correndo()
     
-    if cavalinho6.rect.x < 1084:
+    if cavalinho6.rect.x < 1465:
         cavalinho6.correndo()
