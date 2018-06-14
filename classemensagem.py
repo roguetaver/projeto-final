164633c531e6c7ffa0349fe847d3193076ed5d76
roguetaver <38052553+roguetaver@users.noreply.github.com>
2018-06-13 23:58:07 -0300

    classe mensagem

--- classemensagem.py
+++ classemensagem.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May  8 14:51:21 2018
+Created on Tue May  8 17:05:50 2018
 
-@author: mihan
+@author: andre
 """
+
 import pygame
 
 class mensagem(pygame.sprite.Sprite):
     
-    def __init__(self, arquivo_imagem, pos_x, pos_y, tamx, tamy):
+    def __init__(self, arquivo_imagem, pos_x, pos_y):
         pygame.sprite.Sprite.__init__(self)
         imagem=pygame.image.load(arquivo_imagem)
-        self.image = pygame.transform.scale(imagem,(tamx, tamy))
+        self.image = pygame.transform.scale(imagem,(600,600))
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
